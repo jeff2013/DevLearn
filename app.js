a30449642488145f571a87c6688caee3449aeccc
reedmullanix <reedmullanix@gmail.com>
2016-02-27 13:09:15 -0800

    Initial db setup

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var path = require('path');
+var fs = require('fs');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -23,7 +27,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+
+var models = require('./models');
+models.sequelize.sync().then(function () {
+  console.log('Sequelize initialized!')
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
