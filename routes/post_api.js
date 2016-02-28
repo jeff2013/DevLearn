8de65044ce87cd9c1582d2567e746946d80bf5a8
reedmullanix <reedmullanix@gmail.com>
2016-02-28 05:29:38 -0800

    Finished off post api

--- routes/post_api.js
+++ routes/post_api.js
@@ -19,15 +19,44 @@ router.get('/:post_id', function (req, res) {
 
 router.post('/new_post', function(req, res) {
     var body = req.body;
-    Model.Post.create({
-        where: {
-            title: body.title,
-            type: body.type,
-            content: body.content,
-            popularity: 0,
-            
-        }
-    })
+    if(req.isAuthenticated()) {
+        // Find the user details
+        Model.User.findOne({
+            where: { username: req.user.username}
+        }).then(function(user) {
+           return user.addPost({
+               where: {
+                   title: body.title,
+                   type: body.type,
+                   content: body.content,
+                   popularity: 0,
+                   image_url: body.image_url
+               }
+           })
+        }).then(function(post) {
+            return Promise.all([Promise.resolve(post)]
+                .concat(
+                    body.tags.map(function (tag) {
+                        return Model.Tag.findOrCreate({
+                            where: {
+                                title: tag
+                            }
+                        })
+                    })
+                )
+            )
+        }).then(function(tags) {
+            var post = tags.unshift()
+            return post.setTags(tags)
+        }).then(function(){
+            res.json({ result: post.dataValue })
+        }).catch(function(err){
+            console.error(err);
+            res.status(500).end();
+        });
+    } else {
+        res.status('403').end();
+    }
 });
 
 module.exports = router;
