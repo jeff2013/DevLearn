a6b9c8168d5a8b923c08469982ea50bc3f8f86f3
Jeff Chang <jeffchang@dhcp-206-87-213-17.ubcsecure.wireless.ubc.ca>
2016-02-28 05:44:34 -0800

    Added individual post pages

--- models/post.js
+++ models/post.js
@@ -15,7 +15,7 @@ module.exports = function (sequelize, DataTypes) {
     }, {
         classMethods: {
             associate: function (models) {
-                Post.belongsTo(models.User, { as: 'Post', foreignKey: 'user_id'});
+                Post.belongsTo(models.User , { as: 'Post', foreignKey: 'user_id'});
                 Post.belongsToMany(models.Tag, { through : "PostTags" });
             }
         }
